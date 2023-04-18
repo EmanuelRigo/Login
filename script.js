cba551cf026ca4c6bcba07340f71dfcaf102494a
emanuelrigo <112441019+EmanuelRigo@users.noreply.github.com>
2023-04-18 17:54:32 -0300

    javascript

--- script.js
+++ script.js
@@ -17,4 +17,41 @@ function login() {
   return ingresar;
 }
 
+if (login()) {
+  let saldo = 200000;
+  let opcion = prompt(
+    "Elegi una opcion: \n1 - Saldo. \n2 - Retiro de dinero. \n3 - Depósito. \nPresiona X para finalizar."
+  );
+
+  while (opcion != "X" && opcion != "x") {
+    switch (opcion) {
+      case "1":
+        alert("tu saldo es $ " + saldo);
+        break;
+      case "2":
+        let retiro = parseInt(prompt("ingresa la cantidad  a retirar"));
+        if (retiro <= saldo) {
+          saldo -= retiro;
+          alert("Retiro exitoso. Tu nuevo saldo es $ " + saldo);
+        } else {
+          alert("fondos insuficientes");
+        }
+        break;
+      case "3":
+        let deposito = parseInt(prompt("ingresa monto a depositar"));
+        saldo += deposito;
+        alert("Deposito exitoso, su nuevo saldo es $ " + saldo);
+        break;
+      default:
+        alert("elegiste una opcion invalida");
+        break;
+    }
+    opcion = prompt(
+      "Elegi una opcion: \n1 - Saldo. \n2 - Retiro de dinero. \n3 - Depósito. \nPresiona X para finalizar."
+    );
+  }
+} else {
+  alert("nos quedaremos con tu tarjeta");
+}
+alert("adios");
 login();
